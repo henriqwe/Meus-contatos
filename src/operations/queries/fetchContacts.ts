7e14af7494748d71cd903406e969c9848abfd2e1
henriqwe <henrqiwe96@gmail.com>
2023-08-14 15:15:18 -0300

    feat: exibindo os contatos após query

---
+++ src/operations/queries/fetchContacts.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+interface IContact {
+  id: number
+  name: string
+  username: string
+  email: string
+  address: {
+    street: string
+    suite: string
+    city: string
+    zipcode: string
+    geo: {
+      lat: string
+      lng: string
+    }
+  }
+  phone: string
+  website: string
+  company: {
+    name: string
+    catchPhrase: string
+    bs: string
+  }
+}
+
+export async function fetchContacts(): Promise<IContact[]> {
+  return axios
+    .get('https://jsonplaceholder.typicode.com/users')
+    .then((res) => res.data)
+}
