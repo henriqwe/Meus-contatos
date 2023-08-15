e5a626e4fb62c75ff171f83760138e2594822525
henriqwe <henrqiwe96@gmail.com>
2023-08-15 19:46:20 -0300

    feat: componente loading

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+export function Loading() {
+  return (
+    <div className="sk-chase">
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+      <div className="sk-chase-dot"></div>
+    </div>
+  )
+}
