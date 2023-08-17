a25f3e4c11b469d52b2209d2022b727a7fd97d88
henriqwe <henrqiwe96@gmail.com>
2023-08-16 21:15:39 -0300

    feat: estilizando scrollbar

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -12,4 +12,19 @@ export const GlobalStyle = createGlobalStyle`
 body{
         background-color: #F1F5F9;
 }
+::-webkit-scrollbar {
+  width: 2px;
+  height: 4px;
+}
+
+/* aqui é para personalizar o fundo da barra, neste caso estou colocando um fundo cinza escuro*/
+::-webkit-scrollbar-track {
+  background: #cacaca ;
+}
+
+/* aqui é a alça da barra, que demonstra a altura que você está na página
+estou colocando uma cor azul clara nela*/
+::-webkit-scrollbar-thumb {
+  background: rgba(68, 67, 90, 0.7);
+}
 `
