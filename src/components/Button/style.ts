eb3098aa8720d580f1351530ca60a4d5a01e637f
henriqwe <henrqiwe96@gmail.com>
2023-08-18 00:39:50 -0300

    feat:ajustando estilo dos botoes

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -3,19 +3,31 @@ import styled from 'styled-components'
 const variantOptions = {
   primary: {
     backgroundColor: '#1E40AF',
-    color: 'white'
+    color: 'white',
+    '&:hover': {
+      backgroundColor: '#1a3691'
+    }
   },
   danger: {
     backgroundColor: '#dc2626',
-    color: 'white'
+    color: 'white',
+    '&:hover': {
+      backgroundColor: '#bf2121'
+    }
   },
   success: {
     backgroundColor: '#84cc16',
-    color: 'black'
+    color: 'black',
+    '&:hover': {
+      backgroundColor: '#6fad10'
+    }
   },
   secondary: {
     backgroundColor: '#e2e8f0',
-    color: 'black'
+    color: 'black',
+    '&:hover': {
+      backgroundColor: '#d1d6de'
+    }
   }
 }
 
@@ -24,15 +36,15 @@ export type TVariant = keyof typeof variantOptions
 export const Button = styled.button<{ variant: TVariant }>`
   border: none;
   display: inline-flex;
-  padding-top: 0.5rem;
-  padding-bottom: 0.5rem;
-  padding-left: 0.75rem;
-  padding-right: 0.75rem;
+  padding: 0.8rem;
   justify-content: space-between;
   align-items: center;
   border-radius: 0.375rem;
   font-weight: 500;
+  transition: all 0.5s;
   cursor: pointer;
+  outline: none;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.25);
   ${({ variant }) => variantOptions[variant]}
 `
 export const ButtonContainer = styled.div`
